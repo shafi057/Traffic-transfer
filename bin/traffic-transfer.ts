aaa7d26423280c29c073204511a11bd97411f5cf
Samiksha022628 <samiksha022628@gmail.com>
2025-05-23 11:42:37 +0530

    testing

--- bin/traffic-transfer.ts
+++ bin/traffic-transfer.ts
@@ -14,7 +14,7 @@ new EksIstioStack(app, 'EksIstioStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '533267034674', region: 'us-east-1' },
+  env: { account: '058264400099', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/eks-istio-stack.ts
+++ lib/eks-istio-stack.ts
@@ -2,111 +2,94 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as yaml from 'js-yaml';
+import * as yaml from 'yaml';
 import { KubectlV28Layer } from '@aws-cdk/lambda-layer-kubectl-v28';
 
 export class EksIstioStack extends Stack {
-  constructor(scope: cdk.App, id: string, props?: StackProps) {
-    super(scope, id, props);
+constructor(scope: cdk.App, id: string, props?: StackProps) {
+super(scope, id, props);
 
-    // IAM Role for EKS Admin
-    const adminRole = new iam.Role(this, 'EksAdminRole', {
-      assumedBy: new iam.AccountRootPrincipal(),
-    });
+const adminRole = new iam.Role(this, 'EksAdminRole', {
+  assumedBy: new iam.AccountRootPrincipal(),
+});
 
-    adminRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSClusterPolicy'));
+adminRole.addManagedPolicy(
+  iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSClusterPolicy')
+);
 
-    // Add KubectlLayer for CDK Lambda-backed kubectl
-    const kubectlLayer = new KubectlV28Layer(this, 'KubectlLayer');
+const kubectlLayer = new KubectlV28Layer(this, 'KubectlLayer');
 
-    // Create EKS Cluster
-    const cluster = new eks.Cluster(this, 'EksCluster', {
-      version: eks.KubernetesVersion.V1_28,
-      defaultCapacity: 2,
-      mastersRole: adminRole,
-      kubectlLayer: kubectlLayer,
-    });
+const cluster = new eks.Cluster(this, 'EksCluster', {
+  version: eks.KubernetesVersion.V1_28,
+  defaultCapacity: 0,
+  mastersRole: adminRole,
+  kubectlLayer,
+});
 
-    cluster.awsAuth.addRoleMapping(adminRole, {
-        groups: ['system:masters']
-      })
+const nodegroup = cluster.addNodegroupCapacity('NodeGroup', {
+  desiredSize: 2,
+  instanceTypes: [new ec2.InstanceType('t3.medium')],
+  remoteAccess: { sshKeyName: 'demo' },
+});
 
-    // Create Istio namespace
-    const istioNamespace = cluster.addManifest('IstioNamespace', {
-      apiVersion: 'v1',
-      kind: 'Namespace',
-      metadata: { name: 'istio-system' },
-    });
+nodegroup.role.addManagedPolicy(
+  iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+);
 
-    // Install Istio Helm charts
-    const istioBase = cluster.addHelmChart('IstioBase', {
-      chart: 'base',
-      repository: 'https://istio-release.storage.googleapis.com/charts',
-      release: 'istio-base',
-      namespace: 'istio-system',
-    });
+cluster.awsAuth.addRoleMapping(nodegroup.role, {
+  username: 'system:node:{{EC2PrivateDNSName}}',
+  groups: ['system:bootstrappers', 'system:nodes', 'system:masters'],
+});
 
-    const istiod = cluster.addHelmChart('Istiod', {
-      chart: 'istiod',
-      repository: 'https://istio-release.storage.googleapis.com/charts',
-      release: 'istiod',
-      namespace: 'istio-system',
-      values: {
-        global: {
-          istioNamespace: 'istio-system',
-        },
-      },
-    });
+// Create required namespaces inline
+const istioNamespace = cluster.addManifest('IstioNamespace', {
+  apiVersion: 'v1',
+  kind: 'Namespace',
+  metadata: { name: 'istio-system' },
+});
 
-    const istioIngress = cluster.addHelmChart('IstioIngress', {
-      chart: 'gateway',
-      repository: 'https://istio-release.storage.googleapis.com/charts',
-      release: 'istio-ingress',
-      namespace: 'istio-system',
-    });
+const demoNamespace = cluster.addManifest('DemoNamespace', {
+  apiVersion: 'v1',
+  kind: 'Namespace',
+  metadata: { name: 'demo' },
+});
 
-    // Define Helm chart dependencies order
-    istioBase.node.addDependency(istioNamespace);
-    istiod.node.addDependency(istioBase);
-    istioIngress.node.addDependency(istiod);
+// Manifest loader (your exact block)
+const manifestsDir = path.join(__dirname, '..', 'manifests');
+const istioDir = path.join(manifestsDir, 'istio');
 
-    // Load all manifests from the 'manifests' folder
-    const manifestsPath = path.join(__dirname, '..', 'manifests');
+const istioFiles = [
+  'istio-base.yaml',
+  'istio-ingress.yaml',
+  'istiod.yaml'
+];
 
-    const loadManifest = (filename: string) => {
-      const fullPath = path.join(manifestsPath, filename);
-      return yaml.loadAll(fs.readFileSync(fullPath, 'utf8')) as Record<string, any>[];
-    };
+const appFiles = [
+  'demo-v1.yaml',
+  'demo-v2.yaml',
+  'destination-rule.yaml',
+  'gateway.yaml',
+  'virtual-service.yaml'
+];
 
-    const namespaceManifest = loadManifest('namespace.yaml');
-    const demoV1Manifest = loadManifest('demo-v1.yaml');
-    const demoV2Manifest = loadManifest('demo-v2.yaml');
-    const destinationRuleManifest = loadManifest('destination-rule.yaml');
-    const virtualServiceManifest = loadManifest('virtual-service.yaml');
-    const gatewayManifest = loadManifest('gateway.yaml');
+const loadResources = (dir: string, files: string[]) =>
+  files.flatMap(file =>
+    yaml
+      .parseAllDocuments(fs.readFileSync(path.join(dir, file), 'utf-8'))
+      .map((doc: any) => doc.toJSON())
+      .filter(Boolean)
+  );
 
-    // Deploy 'demo' namespace from YAML
-    const demoNamespace = cluster.addManifest('DemoNamespace', ...namespaceManifest);
+const resources = [
+  ...loadResources(manifestsDir, appFiles),
+  ...loadResources(istioDir, istioFiles)
+];
 
-    // Deploy demo app versions
-    const demoAppV1 = cluster.addManifest('DemoAppV1', ...demoV1Manifest);
-    const demoAppV2 = cluster.addManifest('DemoAppV2', ...demoV2Manifest);
-
-    demoAppV1.node.addDependency(demoNamespace);
-    demoAppV2.node.addDependency(demoNamespace);
-
-    // Deploy traffic routing manifests
-    const destinationRule = cluster.addManifest('DestinationRule', ...destinationRuleManifest);
-    const virtualService = cluster.addManifest('VirtualService', ...virtualServiceManifest);
-    const gateway = cluster.addManifest('Gateway', ...gatewayManifest);
-
-    destinationRule.node.addDependency(demoAppV1);
-    destinationRule.node.addDependency(demoAppV2);
-    virtualService.node.addDependency(demoAppV1);
-    virtualService.node.addDependency(demoAppV2);
-    gateway.node.addDependency(demoAppV1);
-    gateway.node.addDependency(demoAppV2);
-  }
+const appManifests = cluster.addManifest('AppManifests', ...resources);
+appManifests.node.addDependency(demoNamespace);
+appManifests.node.addDependency(istioNamespace);
+}
 }
